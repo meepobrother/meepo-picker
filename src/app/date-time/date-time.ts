23804659e78cb06ebfaeeb14679b16cab1feb5a8
15138730370 <1037483576@qq.com>
2017-12-26 11:19:44 +0800

    save to back

--- src/app/date-time/date-time.ts
+++ src/app/date-time/date-time.ts
@@ -50,65 +50,65 @@ export class DateTimeComponent implements OnInit {
     }
 
     ngOnInit() {
-        let times = getTimes();
-        let weeks = getWeeks();
+        let times = this.getTimes();
+        let weeks = this.getWeeks();
         this.cfg.wheels.push({ data: weeks }, { data: times });
         this.picker = new MobileSelect(this.cfg);
     }
-}
 
-export function getWeeks() {
-    let now = new Date();
-    let year = now.getFullYear();
-    let month = now.getMonth() + 1;
-    let weeks = ['周日', '周一', '周二', '周三', '周四', '周五', '周六'];
-    let resultes = [];
-    let day = now.getDate();
-    for (let i = day; i < day + 7; i++) {
-        let time = new Date();
-        let j = 7 - (day + 7) + i;
-        time.setDate(time.getDate() + j);
-        resultes.push({
-            id: `${this.id}-week-${i}`,
-            value: {
-                year: year,
-                month: month,
-                day: time.getDay()
-            },
-            title: weeks[time.getDay()]
-        });
+    getTimes() {
+        // 从现在到24点
+        let times: any[] = [];
+        let now = new Date();
+        let hour = now.getHours();
+        let minute = now.getMinutes();
+        for (let i = hour; i < 24; i++) {
+            let i_str = this.toTenStr(i);
+            let minutesItems = [0, 15, 30, 45];
+            minutesItems.map(j => {
+                let j_str = this.toTenStr(j);
+                times.push({
+                    id: `${this.id}-time-${i}`,
+                    value: {
+                        hour: i,
+                        minute: j
+                    },
+                    title: `${i_str}:${j_str}`
+                })
+            });
+        }
+        return times;
     }
-    return resultes;
-}
 
-export function toTenStr(i) {
-    if (i < 10) {
-        return `0${i}`;
-    } else {
-        return `${i}`;
+    toTenStr(i) {
+        if (i < 10) {
+            return `0${i}`;
+        } else {
+            return `${i}`;
+        }
     }
-}
 
-export function getTimes() {
-    // 从现在到24点
-    let times: any[] = [];
-    let now = new Date();
-    let hour = now.getHours();
-    let minute = now.getMinutes();
-    for (let i = hour; i < 24; i++) {
-        let i_str = toTenStr(i);
-        let minutesItems = [0, 15, 30, 45];
-        minutesItems.map(j => {
-            let j_str = toTenStr(j);
-            times.push({
-                id: `${this.id}-time-${i}`,
+    getWeeks() {
+        let now = new Date();
+        let year = now.getFullYear();
+        let month = now.getMonth() + 1;
+        let weeks = ['周日', '周一', '周二', '周三', '周四', '周五', '周六'];
+        let resultes = [];
+        let day = now.getDate();
+        for (let i = day; i < day + 7; i++) {
+            let time = new Date();
+            let j = 7 - (day + 7) + i;
+            time.setDate(time.getDate() + j);
+            resultes.push({
+                id: `${this.id}-week-${i}`,
                 value: {
-                    hour: i,
-                    minute: j
+                    year: year,
+                    month: month,
+                    day: time.getDay()
                 },
-                title: `${i_str}:${j_str}`
-            })
-        });
+                title: weeks[time.getDay()]
+            });
+        }
+        return resultes;
     }
-    return times;
 }
