c1f823b05e4ebd6e23fadaa9432b1cbbc9b40fbc
15138730370 <1037483576@qq.com>
2018-01-12 11:45:16 +0800

    save to back

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DateTimeComponent } from './date-time/date-time';
 import { UuidModule } from 'meepo-uuid';
+import { ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
     declarations: [
         DateTimeComponent
     ],
-    imports: [ CommonModule, UuidModule ],
+    imports: [
+        CommonModule,
+        UuidModule,
+        ReactiveFormsModule
+    ],
     exports: [
         DateTimeComponent
     ],
     providers: [],
 })
-export class PickerModule {}
+export class PickerModule { }
---
+++ src/app/time-picker/time-picker.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit, forwardRef, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+export const EXE_COUNTER_VALUE_ACCESSOR: any = {
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => TimePickerComponent),
+    multi: true
+};
+@Component({
+    selector: 'time-picker',
+    templateUrl: './time-picker.html',
+    styleUrls: ['./time-picker.scss'],
+    encapsulation: ViewEncapsulation.None
+})
+export class TimePickerComponent implements OnInit, ControlValueAccessor {
+    @Input() isCoach: boolean = false;
+
+    _model: any;
+    set model(val: any) {
+        this._model = val;
+        this.propagateChange(this._model);
+    }
+    get model() {
+        return this._model;
+    }
+    constructor() { }
+
+    ngOnInit() { }
+
+    doNow() {
+        this.isCoach = false;
+    }
+
+    doCoach() {
+        this.isCoach = true;
+    }
+
+    timePicker(e: any) {
+        this.model = e;
+    }
+
+    propagateChange = (_: any) => { };
+
+    writeValue(value: any) {
+        if (value) {
+            this.model = value;
+        }
+    }
+
+    registerOnChange(fn: any) {
+        this.propagateChange = fn;
+    }
+
+    registerOnTouched(fn: any) { }
+}
